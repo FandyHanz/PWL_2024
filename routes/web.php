13a24bf527421388626375182508b11baebbe869
Fandy Hanz <hanzyura28@gmail.com>
2025-02-19 09:30:13 +0700

    P2_Fandy Wahyu Hanzura_Percobaan1

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function (){
+    return 'World';
+});
+
+Route::get('/welcome_message', function(){
+    return 'selamat datang';
+});
+
+Route::get('/about', function(){
+    return '2341720165';
+});
+
+Route::get('/user/{name}', function($name){
+    return 'nama saya ' . $name;
+});
+
+Route::get('/post/{post}/comment/{comment}', function($postId, $commentId){
+    return 'posts ke- ' . $postId . "komentar ke- " . $commentId;
+});
+
+Route::get('/article/{article}', function($articleId){
+    return 'article id- {'. $articleId . '}';
+});
+
+Route::get('/name/{name?}', function($name = 'John'){
+    return 'Nama saya - ' . $name;
 });
